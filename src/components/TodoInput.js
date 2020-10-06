bdc121cf432b89542938339f929234ed624d8786
matthias haefeli <haefeli.matthias@gmail.com>
2020-10-06 15:59:19 -0600

    add input form

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBook } from '@fortawesome/free-solid-svg-icons'
 
 class TodoInput extends Component {
   render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
+
     return (
-      <div>
-        TodoInput
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <FontAwesomeIcon icon={faBook} />
+              </div>
+            </div>
+            <input
+              type="text"
+              className="form-control text-capitalize"
+              placeholder="add todo item"
+              value={item}
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-block btn-primary mt-3 text-uppercase">
+            add item
+          </button>
+        </form>
       </div>
     )
   }
